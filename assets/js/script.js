3022f4e2aa7d4f02d9ea8740c2370d84b5ead811
s-shigemat217 <s.shigematsu17@gmail.com>
2025-02-07 17:45:10 +0900

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// 
+const btnStart = document.getElementById('btnStart');
+const btnStop = document.getElementById('btnStop');
+const timer = 
+
+btnStart.addEventListener('click', () => {
+    alert('Startボタンがクリックされました！');
+});
+
+btnStop.addEventListener('click', () => {
+    alert('Stopボタンがクリックされました！');
+});
